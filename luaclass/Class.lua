f08d219cd622e575d02fdc6b26e6348f03d71868
blanhhy <balancestar676@qq.com>
2025-10-31 01:53:45 +0000

    Class接受变长参数作为基类

--- luaclass/Class.lua
+++ luaclass/Class.lua
@@ -6,12 +6,12 @@ local Class
 -- 创建一个元类, 用于适配 lua 经典 OOP 语法
 Class = class "_G::Class"(luaclass) {
     ---@Override,@Classmethod
-    __new = function(self, name, bases)
+    __new = function(self, name, ...)
 		-- 简单起见, 用 Class() 创建的类默认命名空间 _G, 而不是 class
 		if not name:find(':', 1, true) then
 			name = '_G:' .. name
 		end
-        return luaclass:__new(name, bases)
+        return luaclass:__new(name, {...})
     end
 }
 
