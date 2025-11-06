049c818b9ac12f89e946e60a869a95bd59f664be
blanhhy <balancestar676@qq.com>
2025-11-06 14:27:15 +0800

    调整注释

--- demo/classic_luaoop.lua
+++ demo/classic_luaoop.lua
@@ -19,10 +19,11 @@ p:sayHello() -- 输出: Hello, I am Bob
 
 -- 经典语法与 luaclass 语法共用同一套系统, 因此是完全兼容的
 
-class "_G::Student"(Person) {
-  -- 注: 经典语法不指定命名空间默认是 _G
-  -- 而用 luaclass 创建的类命名空间默认是 class
+-- 注:
+-- 经典语法不指定命名空间默认是 _G, 而用 luaclass 创建的类命名空间默认是 class
+-- 不用担心, 因为不论如何显式指定都是有效的
 
+class "_G::Student"(Person) {
   __init = function(self, name, grade)
     super(self):__init(name)
     self.grade = grade
