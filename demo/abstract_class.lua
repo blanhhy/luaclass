343481b4dd42e6207f3f57b493b731c34fb25911
blanhhy <balancestar676@qq.com>
2025-11-09 13:18:13 +0000

    修复声明模式不检查基类字段的bug；添加抽持

--- demo/abstract_class.lua
+++ demo/abstract_class.lua
@@ -2,24 +2,21 @@ require "luaclass"
 
 -- 这个文件简单演示一下luaclass里的抽象类
 --
--- PS: 这并不是我计划中的更新内容, 但是这个特性自己出现了
--- 所以仓促地写了这个 demo
--- 
--- 抽象类是在引入声明模式之后自然出现的
+-- 抽象类是在引入声明模式之后自然出现的, 本来并不是计划中的更新内容
 -- 具体来说, decl 占位符中有一个函数类型, 写法是 decl.method
 -- 
--- 让我们回顾一下声明模式的特点: 
--- 声明的字段必须在实例化前初始化为正确的类型, 否则在实例化时报错
--- 
--- 一般在初始化发生在__init中, 但是, 子类的函数重写也可以实现这一点
--- 所以含有 decl.method 的基类不能直接实例化, 而实现了这个方法的子类可以实例化
+-- 含有 decl.method 的类会因为声明字段初始化检查而不能实例化, 而实现了这个方法的子类则可以
 -- 这时候 decl.method 就成了抽象方法, 而这个类自然成了抽象类
+-- 
+-- 现在, 我添加了单独的 abstract 字段, 不必依赖声明模式
+-- 声明了 abstract = true 的类会成为抽象类, 无法实例化
+-- 抽象类的子类必须要么也是抽象类, 要么实现所有抽象方法, 否则在定义类时报错
 
 
--- 这是一个抽象的Animal类
--- 它不能实例化, 因为 eat 和 move 方法待实现
+-- 这是一个抽象的Animal类, 它不能实例化, 并且有 eat 和 move 方法待实现
 class "_G::Animal" {
-	declare = true; -- 要使用抽象类, 必须打开声明模式
+--  declare = true; -- 不再对抽象类必要
+	abstract = true;
 	eat  = decl.method;
 	move = decl.method;
 }
@@ -27,19 +24,17 @@ class "_G::Animal" {
 -- 飞行能力抽象类
 -- fly 方法待实现
 class "_G::CanFly" {
-	declare = true;
+	abstract = true;
 	fly = decl.method;
 }
 
 
 class "_G::Sheep"(Animal) {
-	declare = true; -- 不写也可以, 因为会从父类继承, 但是写了更清晰
-	
 	---@Override
 	eat = function(self)
 		print(self:getClass():toString().." eats grass.")
 	end;
-	
+
 	---@Override
 	move = function(self)
 		print(self:getClass():toString().." walks on legs.")
@@ -48,18 +43,16 @@ class "_G::Sheep"(Animal) {
 
 -- 多继承, 有疑问的参考MRO的那个demo
 class "_G::Sparrow"(Animal, CanFly) {
-	declare = true;
-	
 	---@Override
 	eat = function(self)
 		print(self:getClass():toString().." eats insects.")
 	end;
-	
+
 	---@Override
 	move = function(self)
 		return self:fly() -- 简单起见, 这里直接委托 fly 方法
 	end;
-	
+
 	---@Override
 	fly = function(self)
 		print(self:getClass():toString().." flys.")
@@ -76,9 +69,15 @@ sparrow:eat()
 sparrow:move()
 
 --[[
-输出: 
+输出:
 Sheep eats grass.
 Sheep walks on legs.
 Sparrow eats insects.
 Sparrow flys.
 ]]
+
+
+-- 错误示范:
+class "_G::Dog" (Animal) {}
+-- 报错, 因为Dog类既没有声明为抽象类也没有实现基类的eat和move方法
+-- class '_G::Dog' is not abstract and does not override abstract method 'eat'
--- luaclass/core/class.lua
+++ luaclass/core/class.lua
@@ -10,6 +10,7 @@ local namespace = _G.require("luaclass.core.namespace")
 local declare   = _G.require("luaclass.core.declare")
 
 
+-- 判断或获取所属类型
 local function isinstance(obj, cls)
   local typ = type(obj)
   local obj_cls = typ == "table" and obj.__class
@@ -27,37 +28,31 @@ end
 
 -- 类的实例化
 local function new_instance(cls, ...)
-  local inst = cls:__new(...) -- 调用构造函数
-  local declared, decl_count
 
-  -- 类是否使用声明模式
-  if cls.declare then
-    declared, decl_count = {}, 0
-    for k, v in next, cls do
-      if declare.type[v] then
-        declared[decl_count + 1] = k
-        decl_count = decl_count + 1
-      end
-    end
+  -- 抽象类不能实例化！
+  if cls.abstract then
+    _G.error(("Cannot instantiate abstract class '%s'")
+      :format(cls.__classsgin), 2)
   end
-  
-  -- 如果有初始化函数，调用它
-  local init = cls.__init
+
+  local inst = cls:__new(...) -- 调用构造函数
+  local init = cls.__init -- 如果有初始化函数，调用它
   if init then init(inst, ...) end
 
-  -- 在声明模式下, 检查是否有未初始化字段以及类型匹配
-  if declared then
-    local field, value -- 字段名和初始化后的值
-    for i = 1, decl_count do
-      field = declared[i]
-      value = rawget(inst, field)
+  -- 如果声明了字段, 检查是否正确初始化
+  if cls.declare then
+    local declared = cls.__declared -- 所有声明字段
+    local decltype = declare.type -- 获取声明类型
+    local value
+    for field, decl in next, declared do
+      value = inst[field]
       if nil == value then
         _G.error(("Uninitialized declared field '%s' in instance of class '%s'")
-          :format(field.. ": " ..declare.type[cls[field]], cls.__ns_name.. "::" ..cls.__classname), 2)
+          :format(field.. ": " ..decltype[decl], cls.__classsgin), 2)
       end
-      if not isinstance(value, declare.type[cls[field]]) then
+      if not isinstance(value, decltype[decl]) then
         _G.error(("Initializing declared field '%s' with a %s value in instance of class '%s'")
-          :format(field.. ": " ..declare.type[cls[field]], isinstance(value), cls.__ns_name.. "::" ..cls.__classname), 2)
+          :format(field.. ": " ..decltype[decl], isinstance(value), cls.__classsgin), 2)
       end
     end
   end
@@ -65,17 +60,21 @@ local function new_instance(cls, ...)
   return inst
 end
 
+
 local luaclass = {
   __classname  = "luaclass";
   __ns_name    = "class";
+  __classsgin  = "class::luaclass";
   __tostring   = function(self) return self.__classname or "<anonymous>" end;
   __call       = new_instance;
   __index      = fromsuper;
+--__newindex   = reflect;
 } -- 基本元类
 
 local Object   = {
   __classname  = "Object";
   __ns_name    = "class";
+  __classsgin  = "class::Object";
   __tostring   = function(self) return ("<%s object>"):format(self.__class.__classname) end;
   __new        = function(self) return setmetatable({__class = self}, self) end;
   isInstanceOf = isinstance;
@@ -102,11 +101,11 @@ local class_NS = namespace.new(
 "class", {
   luaclass = luaclass;
   Object   = Object;
-  
+
   isinstance = isinstance;
   namespace  = namespace;
   decl       = declare;
-  
+
   std = _G;
 })
 
@@ -117,12 +116,13 @@ local mm_names = {
   "__concat", "__len", "__eq", "__lt", "__le", "__call", "__gc"
 }
 
+
 -- 创建一个类
-function luaclass.__new(mcls, ...)
+function luaclass:__new(...)
   local arg_count = select('#', ...)
 
   if arg_count == 0 then
-    _G.error("bad argument #1 to 'luaclass.__new' (value expected)", 3)
+    _G.error("bad argument #1 to 'luaclass:__new' (value expected)", 3)
   end
 
   -- 单参数调用时，返回对象的类
@@ -145,6 +145,7 @@ function luaclass.__new(mcls, ...)
   local cls = {
     __classname = name;
     __ns_name   = ns_name;
+    __classsgin = ns_name.. "::".. name;
     __class     = mcls;
     __new       = Object.__new; -- 这个方法比较常用
     __tostring  = Object.__tostring;
@@ -158,13 +159,15 @@ function luaclass.__new(mcls, ...)
       cls[k] = v
     end
   end
+  
+  local as_abc = cls.abstract
 
   -- 计算MRO
   local mro, err = mergeMROs(cls, bases)
   if err then _G.error(err, 3) end
 
   cls.__mro = mro
-  setmetatable(cls, mcls)
+  setmetatable(cls, self) -- 元类是类的元表
 
   -- 由于 Lua 不从 __index 中查找元方法
   -- 所以要继承元方法只好从基类中复制
@@ -177,6 +180,25 @@ function luaclass.__new(mcls, ...)
       cls[mm_name] = base_mm
     end
   end
+  
+  -- 如果基类抽象而子类不抽象
+  -- 检查子类是否实现了所有的方法, 否则无法创建类
+  if not as_abc and nil ~= cls.abstract then
+    local abms, method
+    for i = 1, #bases do
+      abms = bases[i].__abstract_methods
+      if abms then
+        for j = 1, #abms do
+          method = cls[abms[j]]
+          if method == declare.method or type(method) ~= "function" then
+            _G.error(("class '%s' is not abstract and does not override abstract method '%s'")
+              :format(cls.__classsgin, abms[j]), 3)
+          end
+        end
+      end
+    end
+    cls.abstract = false
+  end
 
   -- 注册类到对应的命名空间
   local ns = namespace.get(ns_name)
@@ -186,6 +208,28 @@ function luaclass.__new(mcls, ...)
 end
 
 
+-- 辅助函数: 获取声明字段
+local function getDeclared(clstb, bases)
+  local declared = {}
+  
+  if bases then
+    for i = 1, #bases do
+      if bases[i].__declared then
+        for k, v in next, bases[i].__declared do
+          declared[k] = v
+        end
+      end
+    end
+  end
+  
+  for k, v in next, clstb do
+    if declare.type[v] then
+      declared[k] = v
+    end
+  end
+  
+  return declared
+end
 
 
 -- 类创建器，用于处理语法
@@ -200,6 +244,22 @@ local function class(name, bases)
 
     local mcls = clstb.metaclass or luaclass -- 支持指定元类，默认 luaclass
     clstb.metaclass = nil
+    
+    -- 声明模式记录静态声明的字段
+    if clstb.declare then
+      clstb.__declared = getDeclared(clstb, bases)
+    end
+    
+    -- 抽象类记录抽象方法
+    if clstb.abstract then
+      clstb.__declared = clstb.__declared or getDeclared(clstb, bases)
+      local abms = {}
+      local method = declare.method
+      for k, v in next, clstb.__declared do
+        if v == method then abms[#abms + 1] = k end
+      end
+      clstb.__abstract_methods = abms
+    end
 
     return mcls(name, bases, clstb) -- 调用元类创建类
   end
