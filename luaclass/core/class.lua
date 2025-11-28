f6470e5bdd27cd864ea032e477feededa90be2bc
blanhhy <balancestar676@qq.com>
2025-11-28 09:15:23 +0800

    修复class.anonymous创建的时机fix

--- luaclass/core/class.lua
+++ luaclass/core/class.lua
@@ -12,6 +12,7 @@ local fromsuper  = require("luaclass.inherit.index")
 local namespace  = require("luaclass.core.namespace")
 local checktool  = require("luaclass.core.checktool")
 local declare    = require("luaclass.share.declare")
+local weaken    = require("luaclass.share.weaktb")
 local randstr    = require("luaclass.share.randstr")
 
 
