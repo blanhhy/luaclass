decf94756a177d602149ea09e7ac983bb05ccb3a
blanhhy <balancestar676@qq.com>
2025-12-10 18:26:59 +0800

    更正插件里一些东西

--- luaclass_ls/luaclass/luaclass.lua
+++ luaclass_ls/luaclass/luaclass.lua
@@ -7,18 +7,18 @@ local _G = _G
 namespace = require "luaclass.core.namespace"
 decl      = require "luaclass.share.declare"
 
-namespace.string = require "string"
-namespace.table  = require "table"
-namespace.math   = require "math"
-namespace.io     = require "io"
-namespace.os     = require "os"
-namespace.debug  = require "debug"
+namespace.string    = require "string"
+namespace.table     = require "table"
+namespace.math      = require "math"
+namespace.io        = require "io"
+namespace.os        = require "os"
+namespace.debug     = require "debug"
 namespace.coroutine = require "coroutine"
-namespace.package = require "package"
-namespace.bit32  = require "bit32"
-namespace.utf8   = require "utf8"
-namespace.ffi    = require "ffi"
-namespace.jit    = require "jit"
+namespace.package   = require "package"
+namespace.bit32     = require "bit32"
+namespace.utf8      = require "utf8"
+namespace.ffi       = require "ffi"
+namespace.jit       = require "jit"
 
 ---@param name? string 类名 (传入空串或缺省则为匿名, 实际类名随机)
 ---@return table
@@ -30,16 +30,16 @@ super      = require "luaclass.inherit.super"
 -- 基本元类
 luaclass = {}
 luaclass.__classname = "luaclass"
-luaclass.__ns_name = "class"
-luaclass.__class = "luaclass"
-luaclass.defaultNS = "class"
+luaclass.__ns_name = "lua.class"
+luaclass.__class = luaclass
+luaclass.defaultns = "lua.class"
 luaclass.typedef = "luaclass"
 
 -- 根类, 所有类都是Object的子类
 Object = {}
 Object.__classname = "Object"
-Object.__ns_name = "class"
-Object.__class = "luaclass"
+Object.__ns_name = "lua.class"
+Object.__class = luaclass
 Object.typedef = "Object"
 Object.toString = tostring
 Object.isInstanceOf = isinstance
