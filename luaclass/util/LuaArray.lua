d602d59971940ecd304060584ca5f0b9ead8fc45
blanhhy <balancestar676@qq.com>
2025-12-17 13:42:43 +0800

    重载比较算符，修复一些问题

--- luaclass/util/LuaArray.lua
+++ luaclass/util/LuaArray.lua
@@ -1,5 +1,3 @@
--- luaclass/util/LuaArray.lua
--- 这个类需要单独导入, require "luaclass" 的时候并不会包含luaclass.util.*
 if not class then
     require "luaclass"
 end
@@ -12,16 +10,19 @@ if isJIT then
     table_clear = require "table.clear"
 end
 
-local type = type
-local tostring = tostring
 local Int = math.floor
-local remove = table.remove
+local type, tostring, setmetatable = type, tostring, setmetatable
+local insert, concat, remove = table.insert, table.concat, table.remove
+local unpack = table.unpack or unpack
+
+local isinstance, luaclass = isinstance, luaclass
 
 class "LuaArray" {
+    -- 静态属性
     ---@static
-    chkidxEnabled = true;
+    chkidxEnabled = true; -- 索引检查开关, 默认开启
 
-    -- 工具函数, 检查索引是否有效
+    -- 静态方法
     ---@static
     ---@param lim number
     ---@param length? integer 如果提供了 length, 返回值会被规范为正向索引
@@ -30,7 +31,7 @@ class "LuaArray" {
         if not LuaArray.chkidxEnabled then return index end
         local err = (
             (type(index) ~= "number" or Int(index) ~= index) and
-            ("<integer> expected, got <%s>."):format(isinstance(index))
+            ("<integer> expected, got <%s>."):format(luaclass(index))
         ) or (
             (index == 0 or index > lim or -index > lim) and
             "Array index out of range"
@@ -40,41 +41,77 @@ class "LuaArray" {
         return index
     end;
 
-    -- 初始化, 填充可能的初始值, 并设置长度
-    ---@Constructor
-    __init = function(self, array)
-        if not array then return end
+    ---@static
+    ---@param length integer
+    ---@param value? any
+    -- 创建一个指定长度的数组, 并填充默认值
+    create = function(length, value)
+        if nil == value then value = 0 end
+        length = LuaArray.chkidx(length, math.huge)
 
-        if type(array) ~= "table" then
-            error(("<table?> expected, got <%s>."):format(isinstance(array)), 2)
+        local arr = LuaArray:__newContainter(length)
+
+        for i = 1, length do
+            arr[i] = value
         end
 
-        local length = 0
-        for i, v in ipairs(array) do
-            self[i] = v
-            length = i
-        end
-        self.length = length
+        return arr
     end;
 
+    -- 构造方法
+    -- 由于 jit 情况下有预分配空间的需求, 所以直接重写了 __new, 没有用 __init
     ---@Override
-    __tostring = function(self)
-        local strList = {}
-        for i = 1, self.length do
-            strList[i] = tostring(self[i])
+    ---@Classmethod
+    __new = function(cls, ...)
+        local nargs = select('#', ...)
+
+        if nargs == 0 then
+            return setmetatable({__class = cls, length = 0}, cls)
         end
-        return '{'..table.concat(strList, ", ")..'}'
+
+        local array = nargs == 1 and type(...) == "table" and (...) or {...}
+        local newArr
+
+        if isJIT then
+            newArr = table_new(#array, 2)
+            newArr.__class = cls
+            setmetatable(newArr, cls)
+        else
+            newArr = setmetatable({__class = cls}, cls)
+        end
+
+        local count = 0
+
+        for i, v in ipairs(array) do
+            newArr[i] = v
+            count = i
+        end
+
+        newArr.length = count
+        return newArr
     end;
 
-    copy = function(self)
-        local copy = LuaArray:__new()
-        for i = 1, self.length do
-            copy[i] = self[i]
+    -- 内部构造方法
+    -- 产生一个空容器, 内部使用, 必须立即填充值
+    ---@Classmethod
+    __newContainter = function(cls, length)
+        local newArr
+
+        if isJIT then
+            newArr = table_new(length, 2)
+            newArr.__class = cls
+            setmetatable(newArr, cls)
+        else
+            newArr = setmetatable({__class = cls}, cls)
         end
-        copy.length = self.length
-        return copy
+
+        newArr.length = length
+        return newArr
     end;
 
+    -- 基本操作方法
+
+    -- 在尾部追加元素
     append = function(self, value)
         if nil == value then
             error("Cannot add nil into a LuaArray", 2)
@@ -83,6 +120,7 @@ class "LuaArray" {
         self[self.length] = value
     end;
 
+    -- 向指定索引处插入元素
     ---@param index? integer 默认在数组末尾
     insert = function(self, index, value)
         index = index and LuaArray.chkidx(index, self.length + 1, self.length) or self.length + 1
@@ -90,9 +128,10 @@ class "LuaArray" {
             error("Cannot add nil into a LuaArray", 2)
         end
         self.length = self.length + 1
-        return table.insert(self, index, value)
+        return insert(self, index, value)
     end;
 
+    -- 弹出指定索引的元素, 返回其值
     ---@param index? integer 默认在数组末尾
     pop = function(self, index)
         index = index and LuaArray.chkidx(index, self.length, self.length) or self.length
@@ -100,42 +139,7 @@ class "LuaArray" {
         return remove(self, index)
     end;
 
-    index = function(self, value)
-        if nil == value then
-            error("non-nil value expected.", 2)
-        end
-        for i = 1, self.length do
-            if value == self[i] then return i end
-        end
-        return nil
-    end;
-
-    lastIndex = function(self, value)
-        if nil == value then
-            error("non-nil value expected.", 2)
-        end
-        for i = self.length, 1, -1 do
-            if value == self[i] then return i end
-        end
-        return nil
-    end;
-
-    indices = function(self, value)
-        if nil == value then
-            error("non-nil value expected.", 2)
-        end
-        local indices = LuaArray:__new()
-        local count = 0
-        for i = 1, self.length do
-            if value == self[i] then
-                count = count + 1
-                indices[count] = i
-            end
-        end
-        indices.length = count
-        return indices
-    end;
-
+    -- 移除指定值, 只移除第一个, 返回其原来的索引
     remove = function(self, value)
         if nil == value then
             error("non-nil value expected.", 2)
@@ -150,6 +154,7 @@ class "LuaArray" {
         return nil
     end;
 
+    -- 移除指定值, 只移除最后一个, 返回其原来的索引
     removeLast = function(self, value)
         if nil == value then
             error("non-nil value expected.", 2)
@@ -164,28 +169,41 @@ class "LuaArray" {
         return nil
     end;
 
+    -- 移除指定值, 移除所有, 返回被移除的个数
     removeAll = function(self, value)
         if nil == value then
             error("non-nil value expected.", 2)
         end
+        local old_length = self.length
         for i = self.length, 1, -1 do
             if value == self[i] then
                 remove(self, i)
                 self.length = self.length - 1
             end
         end
+        return old_length - self.length
     end;
 
-    concat = table.concat;
-    unpack = table.unpack or unpack;
-    ipairs = ipairs;
-    sort   = table.sort;
+    -- 清空数组, 长度归零
+    clear = function(self)
+        if isJIT then
+            table_clear(self, self.length) -- 保留元表
+            self.__class = LuaArray -- 重绑class
+            self.length = 0 -- 重置length
+            return self
+        end
+        for i = 1, self.length do
+            self[i] = nil
+        end
+        self.length = 0
+        return self
+    end;
 
     -- 用另一个数组扩展当前数组
     ---@param array table
     extend = function(self, array)
         if type(array) ~= "table" then
-            error(("<table?> expected, got <%s>."):format(isinstance(array)), 2)
+            error(("<table?> expected, got <%s>."):format(luaclass(array)), 2)
         end
         local length = self.length
         local count = 0
@@ -197,37 +215,20 @@ class "LuaArray" {
         return self
     end;
 
-    -- 连接两个数组, 得到一个新的数组
-    __concat = function(arr1, arr2)
-        if not isinstance(arr1, LuaArray) or not isinstance(arr2, LuaArray) then
-            error(("attempt to concat LuaArray with a %s value")
-                :format(isinstance(arr1, LuaArray) and luaclass(arr2) or luaclass(arr1)), 2)
+    -- 填充数组的某块区域为指定值
+    fill = function(self, value, i, j)
+        if nil == value then
+            error("Cannot add nil into a LuaArray", 2)
         end
-
-        local newArr
-        local length = arr1.length + arr2.length
-
-        if isJIT then
-            newArr = table_new(length, 2)
-            newArr.__class = LuaArray
-            setmetatable(newArr, LuaArray)
-        else
-            newArr = LuaArray:__new()
+        i = i and LuaArray.chkidx(i, self.length, self.length) or 1
+        j = j and LuaArray.chkidx(j, self.length, self.length) or self.length
+        for o = i, j do
+            self[o] = value
         end
-
-        newArr.length = length
-
-        for i = 1, arr1.length do
-            newArr[i] = arr1[i]
-        end
-
-        for i = 1, arr2.length do
-            newArr[arr1.length + i] = arr2[i]
-        end
-
-        return newArr
+        return self
     end;
 
+    -- 原地反转数组
     ---@param i? integer
     ---@param j? integer
     reverse = function(self, i, j)
@@ -241,15 +242,76 @@ class "LuaArray" {
         return self
     end;
 
-    ---@param i? integer
-    ---@param j? integer
-    ---@param step? integer
+    -- 查找方法
+
+    -- 找到第一个匹配的索引, 返回 nil 则表示没有找到
+    index = function(self, value)
+        if nil == value then
+            error("non-nil value expected.", 2)
+        end
+        for i = 1, self.length do
+            if value == self[i] then return i end
+        end
+        return nil
+    end;
+
+    -- 找到最后一个匹配的索引, 返回 nil 则表示没有找到
+    lastIndex = function(self, value)
+        if nil == value then
+            error("non-nil value expected.", 2)
+        end
+        for i = self.length, 1, -1 do
+            if value == self[i] then return i end
+        end
+        return nil
+    end;
+
+    -- 找到所有匹配的索引, 返回值是包含所有索引的 LuaArray 对象
+    indices = function(self, value)
+        if nil == value then
+            error("non-nil value expected.", 2)
+        end
+        local indices = setmetatable({__class = LuaArray}, LuaArray) -- 为了简化调用栈, 直接用原始方式了
+        local count = 0
+        for i = 1, self.length do
+            if value == self[i] then
+                count = count + 1
+                indices[count] = i
+            end
+        end
+        indices.length = count
+        return indices
+    end;
+
+    -- 统计一个值出现的次数
+    count = function(self, value)
+        if self.length == 0 or nil == value then return 0 end
+        local count = 0
+        for i = 1, self.length do
+            if value == self[i] then
+                count = count + 1
+            end
+        end
+        return count
+    end;
+
+    -- 切片和复制
+
+    -- 复制数组, 是直接以自己为参数 new 一个新的
+    copy = function(self)
+        return LuaArray:__new(self)
+    end;
+
+    -- 数组切片, 得到一个新的数组
+    ---@param i? integer 起始索引
+    ---@param j? integer 结束索引
+    ---@param step? integer 切片的步长
     sub = function(self, i, j, step)
         i = i and LuaArray.chkidx(i, self.length, self.length) or 1
         j = j and LuaArray.chkidx(j, self.length, self.length) or self.length
         step = step and LuaArray.chkidx(step, math.huge) or 1
 
-        local slice = LuaArray:__new() -- 内部构造函数, 为了下面手动初始化
+        local slice = setmetatable({__class = LuaArray}, LuaArray) -- 内部构造函数, 为了下面手动初始化
         local count = 0
 
         for o = i, j, step do
@@ -271,18 +333,7 @@ class "LuaArray" {
 
         if n <= 0 then return LuaArray() end
 
-        local newArr
-        local length = self.length * n
-
-        if isJIT then
-            newArr = table_new(length, 2)
-            newArr.__class = LuaArray
-            setmetatable(newArr, LuaArray)
-        else
-            newArr = LuaArray:__new()
-        end
-
-        newArr.length = length
+        local newArr = LuaArray:__newContainter(self.length * n)
 
         for i = 1, self.length do
             for j = 0, n - 1 do
@@ -293,27 +344,9 @@ class "LuaArray" {
         return newArr
     end;
 
-    -- 类 Python 的数组重复操作
-    -- eg: arr = LuaArray{1, 2, 3} * 2
-    __mul = function(left, right)
-        if not isinstance(left, LuaArray) then
-            return right:rep(left)
-        end
-        return left:rep(right)
-    end;
-
-    -- 获取值的集合, 顺便包含第一次出现的索引
-    values = function(self)
-        local values = {}
-        for i = self.length, 1, -1 do
-            values[self[i]] = i
-        end
-        return values
-    end;
-
     -- 获取去重数组
     unique = function(self)
-        local unique = LuaArray:__new()
+        local unique = setmetatable({__class = LuaArray}, LuaArray)
         local seen = {}
         local count = 0
         for i = 1, self.length do
@@ -328,59 +361,19 @@ class "LuaArray" {
         return unique
     end;
 
-    clear = function(self)
-        if isJIT then
-            table_clear(self, self.length) -- 保留元表
-            self.__class = LuaArray -- 重绑class
-            self.length = 0 -- 重置length
-            return self
+    -- 获取值的集合, 返回一个 table
+    -- 键是所有的值, 值是它们第一次出现的索引
+    values = function(self)
+        local values = {}
+        for i = self.length, 1, -1 do
+            values[self[i]] = i
         end
-        for i = 1, self.length do
-            self[i] = nil
-        end
-        self.length = 0
-        return self
+        return values
     end;
 
-    ---@static
-    ---@param length integer
-    ---@param value? any
-    -- 创建一个指定长度的数组, 并填充默认值
-    create = function(length, value)
-        if nil == value then value = 0 end
-        length = LuaArray.chkidx(length, math.huge)
+    -- 统计方法
 
-        local arr
-
-        if isJIT then
-            arr = table_new(length, 2)
-            arr.__class = LuaArray
-            setmetatable(arr, LuaArray)
-        else
-            arr = LuaArray:__new()
-        end
-
-        arr.length = length
-
-        for i = 1, length do
-            arr[i] = value
-        end
-
-        return arr
-    end;
-
-    fill = function(self, value, i, j)
-        if nil == value then
-            error("Cannot add nil into a LuaArray", 2)
-        end
-        i = i and LuaArray.chkidx(i, self.length, self.length) or 1
-        j = j and LuaArray.chkidx(j, self.length, self.length) or self.length
-        for o = i, j do
-            self[o] = value
-        end
-        return self
-    end;
-
+    -- 获取数组中的最大值, 要求值可以互相比较
     max = function(self)
         local max = self[1]
         if self.length < 2 then return max end
@@ -390,6 +383,7 @@ class "LuaArray" {
         return max
     end;
 
+    -- 获取数组中的最小值, 要求值可以互相比较
     min = function(self)
         local min = self[1]
         if self.length < 2 then return min end
@@ -399,20 +393,100 @@ class "LuaArray" {
         return min
     end;
 
-    count = function(self, value)
-        if self.length == 0 or nil == value then return 0 end
-        local count = 0
+    -- 转换和比较方法
+
+    ---@Override
+    __tostring = function(self)
+        local strList = {}
         for i = 1, self.length do
-            if value == self[i] then
-                count = count + 1
-            end
+            strList[i] = tostring(self[i])
         end
-        return count
+        return '{'..concat(strList, ", ")..'}'
     end;
+
+    -- 元方法
+
+    -- 连接两个数组, 得到一个新的数组
+    __concat = function(arr1, arr2)
+        if not isinstance(arr1, LuaArray) or not isinstance(arr2, LuaArray) then
+            error(("attempt to concat LuaArray with a %s value")
+                :format(isinstance(arr1, LuaArray) and luaclass(arr2) or luaclass(arr1)), 2)
+        end
+
+        local newArr = LuaArray:__newContainter(arr1.length + arr2.length)
+
+        for i = 1, arr1.length do
+            newArr[i] = arr1[i]
+        end
+
+        for i = 1, arr2.length do
+            newArr[arr1.length + i] = arr2[i]
+        end
+
+        return newArr
+    end;
+
+    -- 类 Python 的数组重复操作
+    -- eg: arr = LuaArray(1, 2) * 3
+    __mul = function(left, right)
+        if not isinstance(left, LuaArray) then
+            return right:rep(left)
+        end
+        return left:rep(right)
+    end;
+
+    -- 重载 < 和 > 符号, 基于数组长度和第一个不等元素
+    __lt = function(left, right)
+        if type(left) ~= "table" or type(right) ~= "table" then -- 允许LuaArray和普通的数组比较
+            error(("attempt to compare LuaArray with a %s value")
+                :format(type(left) == "table" and luaclass(right) or luaclass(left)), 2)
+        end
+        local len1, len2 = left.length or #left, right.length or #right
+        if len1 ~= len2 then return len1 < len2 end
+        for i = 1, len1 do
+            if left[i] < right[i] then return true end
+            if left[i] > right[i] then return false end
+        end
+        return false
+    end;
+
+    -- 重载 <= 和 >= 符号, 基于数组长度和第一个不等元素
+    __le = function(left, right)
+        if type(left) ~= "table" or type(right) ~= "table" then -- 允许LuaArray和普通的数组比较
+            error(("attempt to compare LuaArray with a %s value")
+                :format(type(left) == "table" and luaclass(right) or luaclass(left)), 2)
+        end
+        local len1, len2 = left.length or #left, right.length or #right
+        if len1 ~= len2 then return len1 <= len2 end
+        for i = 1, len1 do
+            if left[i] < right[i] then return true end
+            if left[i] > right[i] then return false end
+        end
+        return true
+    end;
+
+    -- 重载 == 符号, 比较数组内容是否相同
+    __eq = function(left, right)
+        if type(left) ~= "table" or type(right) ~= "table" then -- 允许LuaArray和普通的数组比较
+            return false
+        end
+        local len1, len2 = left.length or #left, right.length or #right
+        if len1 ~= len2 then return false end
+        for i = 1, len1 do
+            if left[i] ~= right[i] then return false end
+        end
+        return true
+    end;
+
+    -- 直接封装的表方法
+    concat = concat;
+    unpack = unpack;
+    ipairs = ipairs;
+    sort   = table.sort;
 }
 
 -- 让切片语法更简洁
--- eg: slice = arr(1, 3)
+-- eg: slice = arr(1, 3[, 1])
 LuaArray.__call = LuaArray.sub
 
 return LuaArray
