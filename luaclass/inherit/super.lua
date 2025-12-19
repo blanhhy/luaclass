deec148db79de8cf29496eec2501ce03db5000e9
blanhhy <balancestar676@qq.com>
2025-12-19 21:16:51 +0800

    优化super逻辑

--- luaclass/inherit/super.lua
+++ luaclass/inherit/super.lua
@@ -1,58 +1,57 @@
-local index = require "luaclass.inherit.index"
+local index  = require "luaclass.inherit.index"
 
-local _G, type, rawget, setmetatable
-    = _G, type, rawget, setmetatable
+local _G, type, setmetatable
+    = _G, type, setmetatable
 
 
--- 拦截并重定向成员访问
-local interceptor = {
-  __index = function(cache, k)
-    local cls = cache[2] -- 访问者代表的子类
-    local field = index(cls, k)
+-- 以某个对象的身份访问它超类上的成员
+-- debug 库可用时, 可以直接 super():foo(), 会自动获取当前方法的 self, 否则必须传入对象
+local super = setmetatable({
+  __index = function(proxy, k)
+    local cls   = proxy.__class -- 子类
+    local field = index(cls, k) -- 从 mro 中找到这个字段
 
     -- 超类中没有这个字段
     if not field then
-      _G.error(("No field or method '%s' existing in superclass of '%s'"):format(k, cls), 2)
+      _G.error(("No field '%s' existing in superclass of '%s'"):format(k, cls), 2)
     end
 
-    -- 找到一个方法
+    -- 是一个方法
     if type(field) == "function" then
-      local function closure(obj, ...)
-        return field(obj == cache and cache[1] or obj, ...)
+      local function proxyMethod(self, ...)
+        return field(self == proxy and proxy.self or self, ...) -- 对象代理方法
       end
-      cache[k] = closure -- 缓存这个闭包
-      return closure
+      proxy[k] = proxyMethod -- 缓存这个闭包到代理表中
+      return proxyMethod
     end
 
     return field -- 普通字段直接返回
-  end
-}
+  end;
 
+  __tostring = function(proxy)
+    return ("<super: %s, %s>"):format(proxy.__class, proxy.self)
+  end;
+}, {
+  __mode = 'k'; -- 弱键模式，对象销毁时清理代理表
 
--- 缓存 super 调用结果
-local supercache = setmetatable({}, {
-  __mode = 'k', -- 弱键模式，对象销毁时清理缓存
-  __index = function(self, obj)
-    local cache = setmetatable({obj,
-      rawget(obj, "__classname") and obj or obj.__class
-    }, interceptor)
-    self[obj] = cache
-    return cache
-  end
+  __call = function(super, obj)
+    if not obj then
+      local _, self
+      if _G.debug then _, self = _G.debug.getlocal(2, 1) end -- 没有传入对象，尝试获取函数第一参数, 即 self
+      obj = self or _G.error("no object provided.", 2) -- 如果没有，抛出一个错误
+    end
+
+    super[obj] = super[obj] or setmetatable({
+      self     = obj;
+      __class  = obj.__class;
+    }, super)
+
+    return super[obj]
+  end;
+
+  __tostring = function()
+    return "super"
+  end;
 })
-
-
-local debug = not not _G.debug
-
--- 以当前身份访问超类字段
-local function super(obj)
-  if not obj then
-    local _, self
-    if _G.debug then _, self = _G.debug.getlocal(2, 1) end -- 如果没有传入类或者对象，尝试获取函数第一参数
-    obj = self or _G.error("Failed to find any class.", 2) -- 如果没有，抛出一个错误
-  end
-  return supercache[obj]
-end
-
 
 return super
