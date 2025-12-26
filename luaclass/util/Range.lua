e146a560a97ee9e2b7392dbbd9d91abef6d2b9dd
blanhhy <balancestar676@qq.com>
2025-12-26 21:47:58 +0800

    添加一个range类

---
+++ luaclass/util/Range.lua
@@ -0,0 +1,59 @@
+if not class then
+    require "luaclass"
+end
+
+local Range
+
+Range = class "lua.class::Range" {
+    ---@Constructor
+    __init = function (self, i, j, step)
+        if i and not j and not step then
+            i, j = 1, i
+        end
+
+        i = i or 1
+        j = j or i
+        step = step or 1
+
+        if not type(i) == "number" then error("bad argument #1, number expected.", 3) end
+        if not type(j) == "number" then error("bad argument #2, number expected.", 3) end
+        if not type(step) == "number" then error("bad argument #3, number expected.", 3) end
+
+        self.START = i
+        self.STOP = j
+        self.STEP = step
+        self.IS_INCREASE = step > 0
+
+        self._i = i
+    end;
+
+    ---@Override
+    __tostring = function (self)
+        return string.format("Range(%d, %d, %d)", self.START, self.STOP, self.STEP)
+    end;
+
+    __len = function (self)
+        if not self.length then
+            self.length = math.floor((self.STOP - self.START) / self.STEP) + 1
+        end
+        return self.length
+    end;
+
+    next = function (self)
+        local current = self._i
+        if self.IS_INCREASE == (current > self.STOP) then -- 同或
+            return nil
+        end
+        self._i = current + self.STEP
+        return current
+    end;
+
+    wind = function (self)
+        self._i = self.START
+        return self
+    end
+}
+
+Range.__call = Range.next -- 确保能被 for 直接调用
+
+return Range
---
+++ luaclass/util/init.lua
@@ -0,0 +1,9 @@
+--
+-- 导入全部 util 类
+
+require "luaclass.util.Class"
+require "luaclass.util.String"
+require "luaclass.util.LuaArray"
+require "luaclass.util.Range"
+
+range = namespace.lua.class.Range
