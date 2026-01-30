14786621e40ea6a098e859f45d98b06cdd935e82
blanhhy <balancestar676@qq.com>
2026-01-30 21:03:18 +0800

    是否包含当前类改为用参数手动确认

--- luaclass/inherit/index.lua
+++ luaclass/inherit/index.lua
@@ -3,10 +3,11 @@ local rawget = rawget
 ---依照MRO查找超类成员
 ---@param self luaclass 类对象
 ---@param name string   成员名
+---@param include_curr? boolean 是否包含当前类
 ---@return any
-return function(self, name)
+return function(self, name, include_curr)
   local mro = self.__mro
-  local start = self == mro[1] and 2 or 1
+  local start = include_curr and 1 or 2
   for i = start, mro.n do
     local item = rawget(mro[i], name)
     if item then return item end
