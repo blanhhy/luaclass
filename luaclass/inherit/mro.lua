c4feec14b7764a0b58f19334d080e4c5a6811c6c
blanhhy <balancestar676@qq.com>
2026-01-31 07:56:23 +0000

    移除mro算法中不必要的部分

--- luaclass/inherit/mro.lua
+++ luaclass/inherit/mro.lua
@@ -21,81 +21,50 @@ return function (cls, bases)
     return {cls, n = 1, lv = {1, n = 1}}
   end
 
-  local mro                      = {cls}  -- 结果MRO
-  local mroLength                = 1      -- 结果MRO长度
-  local mroLengthEachLevels      = {1}    -- 结果MRO的每一层长度
+  local res_mro    = {cls} -- 结果MRO
+  local res_length = 1     -- 结果MRO长度
+  local res_lvsize = {1}   -- 结果MRO的每一层长度
+  local res_nlevel = 1     -- 结果MRO的层数
+  local next_index = {}    -- 各基类MRO的合并进度
+  local seen_pos   = {}    -- 合并过程中已出现过的类位置
 
-  local baseCount                = #bases -- 基类数量
-  local baseMROs                 = {}     -- 各基类的MRO
-  local MROsNextIndextoMerge     = {}     -- 各基类MRO的合并进度
-  local baseMROsLengthEachLevels = {}     -- 储存每个基类的MRO中每一层长度的二维表
-
-  local maxDepth                 = 0      -- 最大继承深度
-  local seenClassesPos           = {}     -- 合并过程中已出现过的类位置
-  local minConflictPos           = 0      -- 最小冲突位置
+  local minConflictPos = 0 -- 最小冲突位置
 
   --[[
     最小冲突位置, 动态更新, 冲突位置大于这个值就向后归并, 但不得小于这个值
     冲突位置, 指的是线性化过程中, 出现了两个相同的类, 前面那个类的位置就是冲突位置
   ]]
 
-  -- 准备工作：从基类中获取各自的MRO和长度，并计算最大继承深度
-  for i = 1, baseCount do
-    local base = bases[i]
-    if not base then baseCount = i break end
-    local baseMRO = base.__mro
+  for level_i = 1, 100 do -- 最大循环100次 (不会有这么深的继承)
+    local merged_count = 0 -- 记录这一层实际合并的数量
+    
+    for base_i = 1, #bases do
+      local curr_mro = bases[base_i].__mro
+      local block_size = curr_mro.lv[level_i]
 
-    -- 获取每个基类的MRO
-    baseMROs[i] = baseMRO
-    MROsNextIndextoMerge[i] = 1 -- 还没有开始合并, 所以下一个位置是1
+      next_index[base_i] = next_index[base_i] or 1
 
-    -- 获取每个基类的MRO中每一层长度的信息
-    baseMROsLengthEachLevels[i] = base.__mro.lv
+      if block_size then
+        local index_to_merge = next_index[base_i]
+        local index_on_merged = index_to_merge + block_size
 
-    -- 计算最大继承深度
-    local baseMRO_Depth = base.__mro.lv.n -- 继承深度, 就是MRO的层级数
-    maxDepth = baseMRO_Depth >= maxDepth
-      and baseMRO_Depth
-      or maxDepth
-  end
-
-  -- 声明一些局部变量
-  local currMRO             -- 当前正在处理的MRO
-  local classGroupCount     -- 组数量：当前MRO在当前层级中包含的超类数量
-  local nextIndextoMerge    -- 当前MRO在开始合并这一层时, 应该从这个位置开始
-  local nextIndexonMerged   -- 预计在合并完这一层之后, 下次应该从这个位置开始
-  local currClass           -- 当前正在处理的超类
-  local pos                 -- 当前超类在结果MRO中的位置
-
-  -- 对于每个基类, 遍历它们的MRO的每个层级, 处理其中的一组超类
-  for whichLevel = 1, maxDepth do
-    local mergedCountInLevel = 0 -- 记录当前层级实际合并的超类数量
-    for whichBase = 1, baseCount do
-
-      currMRO = baseMROs[whichBase]
-      classGroupCount = baseMROsLengthEachLevels[whichBase][whichLevel]
-
-      if classGroupCount then
-        nextIndextoMerge = MROsNextIndextoMerge[whichBase]
-        nextIndexonMerged = nextIndextoMerge + classGroupCount
-
-        -- 开始合并当前定位到的超类组
-        for i = nextIndextoMerge, nextIndexonMerged - 1 do
-          currClass = currMRO[i]
+        -- 开始合并当前定位到的块
+        for i = index_to_merge, index_on_merged - 1 do
+          local curr_cls = curr_mro[i]
 
           -- 如果当前类没有出现过, 直接加入结果MRO中
-          if not seenClassesPos[currClass] then
-            pos = mroLength + 1
+          if not seen_pos[curr_cls] then
+            local pos = res_length + 1
 
-            mro[pos] = currClass -- 加入结果MRO中
-            seenClassesPos[currClass] = pos -- 记录这个类在结果中的位置
+            res_mro[pos] = curr_cls -- 加入结果MRO中
+            seen_pos[curr_cls] = pos -- 记录这个类在结果中的位置
 
-            mergedCountInLevel = mergedCountInLevel + 1 -- 计算当前层级合并的超类数量
-            mroLength = mroLength + 1 -- 记录的长度 +1
+            merged_count = merged_count + 1 -- 计算当前层级合并的超类数量
+            res_length = res_length + 1 -- 记录的长度 +1
 
           else
             -- 如果当前超类已经出现过, 则需要处理可能的冲突
-            local currClassSeenPos = seenClassesPos[currClass] -- 查询这个类之前出现过的位置
+            local conflict_pos = seen_pos[curr_cls] -- 查询这个类之前出现过的位置
             --[[
               单调性错误：
               当 currClassSeenPos < minConflictPos 的时候, 就发生了单调性错误
@@ -105,7 +74,7 @@ return function (cls, bases)
               处理到最后面的A的时候, 发现它已经出现过, 并且 currClassSeenPos = 1, 所以A是不允许的
               直观的理解就是：前面的 A -> B 和后面的 B -> A 冲突了, 无法确认A和B哪个在前哪个在后
             ]]
-            if currClassSeenPos < minConflictPos then -- 发生错误
+            if conflict_pos < minConflictPos then -- 发生错误
 
               -- 放弃合并, 收集信息, 构造错误提示
               local errMsg = "Cannot create class '%s' due to MRO conflict. (in bases: %s, %s)\n"
@@ -115,50 +84,54 @@ return function (cls, bases)
 
               local tostring, concat = _G.tostring, _G.table.concat
 
-              local lastConflictPos   = minConflictPos      -- 上一个冲突位置
-              local currConflictPos   = currClassSeenPos    -- 最新冲突位置
-              local lastConflictClass = mro[minConflictPos] -- 发生冲突的类
-              local currConflictClass = currClass           -- 引发冲突的类
-              local whichChain        = bases[whichBase]    -- 冲突所在的链
-              local whichLevel        = whichLevel          -- 冲突所在的层级
-
-              local mergedMROPath = (function()
+              local prev_seen_pos = minConflictPos
+              local curr_seen_pos = conflict_pos
+              local prev_cls = res_mro[minConflictPos] -- 发生冲突的类
+              
+              local merged_path = (function()
                 local str_list = {cls.__classname}
-                for i = 2, #mro do
-                  str_list[i] = tostring(mro[i])
-                    .. (mro[i] == lastConflictClass and ("@"..lastConflictPos)
-                    or  mro[i] == currConflictClass and ("@"..currConflictPos)
+                for i = 2, #res_mro do
+                  str_list[i] = tostring(res_mro[i])
+                    .. (res_mro[i] == prev_cls and ("@"..prev_seen_pos)
+                    or  res_mro[i] == curr_cls and ("@"..curr_seen_pos)
                     or  "")
                 end
                 local count = #str_list
-                str_list[count+1] = tostring(lastConflictClass)..("@"..(count+1))
-                str_list[count+2] = tostring(currConflictClass)..("@"..(count+2))
+                str_list[count+1] = tostring(prev_cls)..("@"..(count+1))
+                str_list[count+2] = tostring(curr_cls)..("@"..(count+2))
                 return concat(str_list, " -> ")
               end)() -- 已知的 MRO 路径
 
-              return nil,
-              errMsg:format(
+              return nil, errMsg:format(
                 cls.__classname,
-                lastConflictClass,
-                currConflictClass,
-                mergedMROPath,
-                whichChain,
-                whichLevel
+                prev_cls,
+                curr_cls,
+                merged_path,
+                bases[base_i],
+                level_i
               )
             end
             -- 不满足错误条件, 允许冲突, 但不加入MRO, 因为已经存在
-            minConflictPos = currClassSeenPos -- 更新最小冲突位置
+            minConflictPos = conflict_pos -- 更新最小冲突位置
           end
         end
         -- 更新当前基类的MRO合并进度
-        MROsNextIndextoMerge[whichBase] = nextIndexonMerged
+        next_index[base_i] = index_on_merged
       end
     end
-    -- 记录当前层级合并的超类数量
-    mroLengthEachLevels[whichLevel + 1] = mergedCountInLevel
+    
+    -- 实际合并数为0代表已经结束
+    if merged_count == 0 then
+      break
+    end
+
+    res_lvsize[level_i + 1] = merged_count
+    res_nlevel = res_nlevel + 1
   end
-  mro.n = mroLength -- 记录结果MRO长度
-  mroLengthEachLevels.n = 1 + maxDepth -- 记录结果MRO的每一层长度
-  mro.lv = mroLengthEachLevels -- 记录结果MRO的每一层长度
-  return mro
+
+  res_mro.n = res_length
+  res_lvsize.n = res_nlevel
+  res_mro.lv = res_lvsize
+
+  return res_mro
 end
