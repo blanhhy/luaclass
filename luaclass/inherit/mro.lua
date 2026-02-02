8e469fe9b488ce38e71e77ca77c83efadc2f85c1
blanhhy <balancestar676@qq.com>
2026-02-02 19:46:29 +0000

    修复既有实现的bug

--- luaclass/inherit/mro.lua
+++ luaclass/inherit/mro.lua
@@ -3,52 +3,6 @@
 
 local tostring, concat = tostring, table.concat
 
----辅助函数, 寻找好表头
----@param mros {[integer]:luaclass, tail:table<luaclass, boolean>}[]
-local function select_head(mros)
-  local count = #mros -- 要合并的条数
-
-  for i = 1, count do
-    if #mros[i] <= 0 then
-      return nil
-    end
-
-    local head = mros[i][#mros[i]]
-    local is_good = true
-
-    for j = 1, count do
-      if j ~= i and mros[j].tail[head] then
-        is_good = false
-        break
-      end
-    end
-
-    if is_good then
-      for j = 1, count do
-        if mros[j][#mros[j]] == head then
-          mros[j][#mros[j]] = nil -- 弹出表头
-          mros[i].tail[mros[i][#mros[i]] or 0] = nil -- 新表头不再是尾部
-        end
-      end
-      return head
-    end
-  end
-
-  -- 构造错误信息
-  local errMsg = "Cannot create class '%%s' due to MRO conflict. (in bases: %s)\n"
-              .. "Current merged MRO: [%%s]"
-  
-  local bad_heads = {}
-
-  for i = 1, count do
-    local head = mros[i][#mros[i]]
-    if head then bad_heads[#bad_heads+1] = tostring(head) end
-  end
-
-  return nil, errMsg:format(concat(bad_heads, ", "))
-end
-
-
 ---@alias MRO luaclass[]
 
 ---合并基类的 MRO (C3线性化算法)
@@ -73,8 +27,9 @@ return function (cls, bases)
   end
 
   local mros = {}
+  local count = #bases
 
-  for i = 1, #bases do
+  for i = 1, count do
     local base = bases[i]
     local base_mro = base.__mro
     local mro_len = #base_mro
@@ -88,18 +43,49 @@ return function (cls, bases)
   end
 
   while true do
-    local head, err = select_head(mros) -- 反复选择好表头, 直到没有为止
+    local good_head = nil
+    local unfinished = false
+
+    for i = 1, count do
+      if #mros[i] > 0 then
+        unfinished = true
+        good_head = mros[i][#mros[i]]
+        
+        for j = 1, count do
+          if j ~= i and mros[j].tail[head] then
+            good_head = nil break
+          end
+        end
+
+        if good_head then break end
+      end
+    end
     
-    if err then
+    if not good_head and unfinished then
+      local errMsg = "Cannot create class '%s' due to MRO conflict. (in bases: %s)\n"
+                  .. "Current merged MRO: [%s]"
+      local bad_heads = {}
+      for i = 1, count do
+        local head = mros[i][#mros[i]]
+        if head then bad_heads[#bad_heads+1] = tostring(head) end
+      end
       local clsname = cls.__classname or "unknown"
       local mro_str = {clsname}
       for i = 2, #mro do mro_str[i] = tostring(mro[i]) end
-      return nil, err:format(clsname, concat(mro_str, ", "))
+      return nil, err:format(clsname, concat(bad_heads, ", "), concat(mro_str, ", "))
     end
 
-    if not head then break end
+    if not good_head then break end
+
+    for i = 1, count do
+      local mro = mros[i]
+      if mro[#mro] == good_head then
+        mro[#mro] = nil -- 弹出表头
+        mro.tail[mro[#mro] or 0] = nil -- 新表头不再是尾部
+      end
+    end
     
-    mro[#mro + 1] = head
+    mro[#mro + 1] = good_head
   end
 
   return mro
